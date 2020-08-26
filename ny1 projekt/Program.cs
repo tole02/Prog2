d7c2ff51e18255ce7d102248c2249da52615c9f6
Tobias <tobias.lehman@elev.ga.ntig.se>
2020-08-26 11:06:04 +0200

    init

---
+++ ny1 projekt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ny1_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hej = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(hej);
+                hej++;
+            }
+
+            if (hej==5){
+                Console.WriteLine("cool");
+            }
+            Console.ReadLine();
+        }
+    }
+}
